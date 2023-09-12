fcf8f59a974ae7bd1ba328b90522a602a5b2b60e
Leonardo Alves <104588087+Zhorg10@users.noreply.github.com>
2023-09-12 00:04:15 -0300

    voltando para o estado do projeto

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console.log("rodando o sistema de gerenciamento de jogos!")
 console.log("aplicação está conectando ao bando de dados")
-console.log("adicionei mais esse commit")
-console.log("enviando esse commit para o github")
